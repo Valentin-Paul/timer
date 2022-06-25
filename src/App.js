0d96d8adbdae04803d4cd3ec2b56524a4ec2949a
Valentin Paul <valentin@vamptech.de>
2022-06-25 17:00:39 +0200

    added functionality of count down of minutes and seconds

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  let [minutes, setMinutes] = useState(0);
+  let [seconds, setSeconds] = useState(3)
+  var interval = null;
+  let minuteStop = false;
+  let secondStop = false;
+ 
+ 
+   
+
+
+const runTimer = ()=>{
+  console.log(interval)
+  if(secondStop === true && minuteStop === true){
+    console.log("000")
+    clearInterval(startTimer)
+  }
+  else{
+    setSeconds((prevSeconds)=>{
+      if(prevSeconds > 1){
+        secondStop = false
+     return prevSeconds -1
+      }
+      else if(prevSeconds === 1){
+        secondStop = true
+        setMinutes((prevMinutes)=>{
+          if(prevMinutes > 0){
+          return prevMinutes
+          }
+          else if(prevMinutes == 0){
+            console.log(prevMinutes, "brr")
+            minuteStop = true
+            return prevMinutes
+          }
+        })
+        return prevSeconds -1
+      }
+      else if(prevSeconds === 0){
+        setMinutes((prevMinutes)=>{
+      console.log(prevMinutes, "prrrrevMinuuutes")
+            return prevMinutes -1
+          
+        })
+        return 59;
+      }
+    })
+  }
+  }
+
+  let startTimer =()=>{
+    setInterval(runTimer, 1000);
+  } 
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Timer</h1>
+
+        <h3>{minutes === undefined? "00": (minutes === undefined || minutes.toString().length === 1? "0" + minutes : minutes)}:{seconds === undefined || seconds.toString().length === 1? "0" + seconds : seconds}</h3>
+        <button onClick={()=>{startTimer()}}>START</button>
       </header>
     </div>
   );
